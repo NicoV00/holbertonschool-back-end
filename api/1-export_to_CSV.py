a22ead335cf960e4606fa5739d1d511e10208fb7
NicoV00 <6328@holbertonstudents.com>
2023-12-20 14:29:16 -0800

    added fixed task

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """ Task 1 """
 
+
 import csv
 import requests
 import sys
+
 
 def write_file(filename="", rows=None):
     """ Write file """
     with open(filename, mode="a", encoding="utf-8", newline='') as f:
         csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         csv_writer.writerows(rows)
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
